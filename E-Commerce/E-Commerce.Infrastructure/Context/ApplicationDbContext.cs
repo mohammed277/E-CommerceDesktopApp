a9904ddf6f3542338ed4b9396ff5d1a934c16375
Mohammed Khaled <64231779+mohammed277@users.noreply.github.com>
2024-03-18 13:53:56 +0200

    Update ConnectionString

--- E-Commerce/E-Commerce.Infrastructure/Context/ApplicationDbContext.cs
+++ E-Commerce/E-Commerce.Infrastructure/Context/ApplicationDbContext.cs
@@ -12,7 +12,6 @@ namespace E_Commerce.Infrastructure.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-TDQADM2\\AHMEDSABER;Initial Catalog=EcommerceDesktopDB;Integrated Security=True;encrypt=false");
            // optionsBuilder.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=EcommerceDesktopDB;Trusted_Connection=True;MultipleActiveResultSets=true;encrypt=false");
         }
         public DbSet<User> Users { get; set; }
